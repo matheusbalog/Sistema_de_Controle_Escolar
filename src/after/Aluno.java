61a84eac5d62b5d49038d7767bfe20e3ac123310
Matheus Balog <itsbalog.mb@gmail.com>
2026-07-29 17:56:47 -0300

    Refactor

--- src/after/Aluno.java
+++ src/after/Aluno.java
@@ -2,46 +2,28 @@ package after;
 
 public class Aluno {
     private String nome;
-    private int cpf;
-    private int idade;
+    private final Cpf cpf;
+    private final int idade;
     private Curso curso;
     private Nota nota;
 
-    public Aluno(String nome, int cpf, int idade, Nota nota){
+    public Aluno(String nome, Cpf cpf, int idade, Nota nota) {
         this.nome = nome;
         this.cpf = cpf;
         this.idade = idade;
         this.nota = nota;
-
     }
 
+    public String getNome() { return nome; }
+    public Cpf getCpf() { return cpf; }
+    public int getIdade() { return idade; }
+    public Nota getNota() { return nota; }
 
-    //GETTERS
-    public String getNome(){
-        return nome;
-    }
-    public  int getCpf(){
-        return cpf;
-    }
-    public  int getIdade(){
-        return idade;
-    }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setNota(Nota nota) { this.nota = nota; }
+    public void setCurso(Curso curso) { this.curso = curso; }
 
-    //SETTERS
-    public void setCurso(Curso curso){
-        this.curso = curso;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setNota(int nota){
-        this.nota = nota;
-    }
-
-    public boolean isAprovado(){
+    public boolean isAprovado() {
         return nota.isAprovado();
     }
-
-    }
+}
