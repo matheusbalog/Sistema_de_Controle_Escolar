749bcc7a980dc4ea8a42b211a836433f26f04bbf
matheus.balog <itsbalog.mb@gmail.com>
2026-07-29 13:51:23 -0300

    style

---
+++ src/after/Nota.java
@@ -0,0 +1,17 @@
+package after;
+
+public class Nota {
+        private double valor;
+
+        public boolean isAprovado(){
+            return this.valor >=7;
+        }
+       public double getValor(){
+           return valor;
+    }
+
+
+
+
+
+}
