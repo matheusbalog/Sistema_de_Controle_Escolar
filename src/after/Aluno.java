486b7ba0bd38eb34b696c0ef0d113b67c44b9a9d
matheus.balog <itsbalog.mb@gmail.com>
2026-07-30 13:09:40 -0300

    Docs: adiciona comentários

--- src/after/Aluno.java
+++ src/after/Aluno.java
@@ -23,7 +23,7 @@ public class Aluno {
     public void setNome(String nome) { this.nome = nome; }
     public void setNota(Nota nota) { this.nota = nota; }
     public void setCurso(Curso curso) { this.curso = curso; }
-//Só existe 1 único metodo de consulta, além disso não tem else e não imprime nada na tela também
+//Só existe 1 único metodo de consulta, além disso não tem else e não imprimee nada na tela também
     public boolean isAprovado() {
         return nota.isAprovado();
     }
